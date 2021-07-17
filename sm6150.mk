a36582e182243106c7bdbd726e209ed2666f5b11
xNombre <kartapolska@gmail.com>
2021-07-18 00:38:43 +0200

    sm6150-common: Build missing omx libs
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: I3190a4c69c5c930f2760f47417092a2a16319112

--- sm6150.mk
+++ sm6150.mk
@@ -243,9 +243,11 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxCore \
     libOmxEvrcEnc \
+    libOmxG711Enc \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
+    libOmxVidcCommon \
     libstagefrighthw
 
 PRODUCT_COPY_FILES += \

0fd86d24c4f6566e9eb7bf7a31e990cd1d30f059
basamaryan <basam.aryan@gmail.com>
2022-07-02 23:31:25 +0530

    [SQUASH] sm6150-common: Remove moto dolby
    Revert "sm6150-common: audio: Import dolby codecs"
    
    This reverts commit 274b510e5b40290dc7d9f1e3fabb2295ee1510f4.
    
    Revert "sm6150-common: Import media_codecs_vendor.xml from miui_SWEETGlobal_V13.0.8.0.SKFMIXM"
    
    This reverts commit 2098fbd7fc7ba6eeebf7eeba47ba60df66c76cc5.
    
    Revert "sm6150-common: Exclude AudioFx"
    
    This reverts commit 99826664c61e71920ed6d724cef08f85c1f1005e.
    
    Revert "sm6150-common: Import dax-default.xml changes from sweet"
    
    This reverts commit c354c9865140cede7d92ac4e3e55b4774cae4e2a.
    
    Revert "sm6150-common: Add Moto Dolby"
    
    This reverts commit fc833a86e389aff865858208b986f83ab4509f49.
    
    Revert "sm6150-common: audio: Fix echo issues in some cases"
    
    This reverts commit 17f986bc5cf738b8b61d68aae56936d58201543c.
    
    Revert "sm6150-common: audio: Drop sound helper for music and voice"
    
    This reverts commit 008d277a7841977bc2529c3bab0024f4ce2292e4.

--- sm6150.mk
+++ sm6150.mk
@@ -120,13 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
-# Dolby
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
-
-TARGET_EXCLUDES_AUDIOFX := true
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
@@ -242,7 +235,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_sdmmagpie_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_sdmmagpie_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml

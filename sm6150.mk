818d920c7f4f620337e1b545251d25deaae9a9ff
zaidkhan0997 <kzaid0997@gmail.com>
2023-04-05 18:13:26 +0530

    sm6150-common: BringUp PixelExtended
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
@@ -311,10 +311,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
-
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
 
 # Sensors
 PRODUCT_COPY_FILES += \
